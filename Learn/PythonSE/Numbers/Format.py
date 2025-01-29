2639bf09979356d7d34542fa19e0a34477c301ea
gibran <zaga1709@hotmail.com>
2025-01-28 21:53:36 -0600

    Nuevos temas en String

---
+++ Learn/PythonSE/Numbers/Format.py
@@ -0,0 +1,9 @@
+# Concatenar
+a = "Hola"
+b = "Juan"
+
+c = a + b
+print(c)
+
+c = a + " " + b
+print(c)
---
+++ Learn/PythonSE/Strings/ModifyStrings.py
@@ -0,0 +1,18 @@
+# Upper Case
+a = "Mi precioso"
+print(a.upper())
+
+# Lower Case
+a = "Feliz NAVIDAD"
+print(a.lower())
+
+# Remove Whitespace from the beginning
+a = "   Ayuda, por favor!"
+print(a.strip())
+
+# Reemplazar
+print(a.replace("Ayuda", "Auxilio").strip())
+
+# Separar en lista
+a = "Hello, World!"
+print(a.split(","))
